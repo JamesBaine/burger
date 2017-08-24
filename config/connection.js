0f0b23408da3aaa676225ac211c52195aff246c1
JamesBaine <jamesmbaine@gmail.com>
2017-08-23 17:41:50 -0700

    connection variable rearranged

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,6 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "burgers_db"
-});
+var connection;
+
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
