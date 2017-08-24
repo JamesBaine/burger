ed59865b3b194999c8679cfbf07ac3337a60751c
JamesBaine <jamesmbaine@gmail.com>
2017-08-23 17:03:13 -0700

    connected to JawsDB

--- config/connection.js
+++ config/connection.js
@@ -5,6 +5,16 @@ var connection = mysql.createConnection({
 	password: "",
 	database: "burgers_db"
 });
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "",
+		database: "burgers_db"
+	});
+}
 
 connection.connect(function(err){
 	if(err)
