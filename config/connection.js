6d5d965707cc69a29c340968e6fcdd73f3393fee
JamesBaine <jamesmbaine@gmail.com>
2017-08-31 17:06:34 -0700

    heroku connection hopefully fixed

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_URL) {
+if(process.env.JAWSDB_URL) {
+	//Heroku deployment
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+	//local host
 	connection = mysql.createConnection({
+		root: 3000,
 		host: "localhost",
 		user: "root",
 		password: "",
-		database: "burgers_db"
+		database: "xqcjdeftxxwmr7ln",
 	});
 }
 
